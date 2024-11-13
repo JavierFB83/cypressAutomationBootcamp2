43533f7b9bae2fd66c8b843c5422c04d7658f9ed
Javier Flores <javi.flores@plytix.com>
2024-11-13 20:00:06 +0100

    mocha report

--- cypress.config.js
+++ cypress.config.js
@@ -2,26 +2,45 @@ const { addCucumberPreprocessorPlugin } = require('@badeball/cypress-cucumber-pr
 const { createEsbuildPlugin } = require('@badeball/cypress-cucumber-preprocessor/esbuild');
 const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
 const { defineConfig } = require('cypress');
+const cypressOnFix = require('cypress-on-fix');
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
+  projectId: 'ncsghx',
   e2e: {
-    async setupNodeEvents(on, config) {
+    setupNodeEvents: async (on, config) => {
+      // "cypress-on-fix" is required because "cypress-mochawesome-reporter" and "cypress-cucumber-preprocessor" use the same hooks
+      on = cypressOnFix(on);
+      
+      // Mochawesome reporter plugin
+      require('cypress-mochawesome-reporter/plugin')(on);
+      
+      // Cucumber plugin
       await addCucumberPreprocessorPlugin(on, config);
+      
+      // Esbuild preprocessor plugin
       on(
         'file:preprocessor',
         createBundler({ plugins: [createEsbuildPlugin(config)] })
       );
+
       return config;
     },
-    specPattern: ['**/*.feature','**/apiTest/*/*.js'],
-    chromeWebSecurity: false,
     failOnStatusCode: false,
-    numTestsKeptInMemory: 3,
+    chromeWebSecurity: false,
+    specPattern: ['**/*.feature', '**/apiTests/*/*.js'],
     defaultCommandTimeout: 10000,
+    numTestsKeptInMemory: 10,
     env: {
       snapshotOnly: true,
-      requestMode: true
+      requestMode: true,
     },
   },
-
 });
